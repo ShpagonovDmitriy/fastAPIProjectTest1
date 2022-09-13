7508771d6cabce7c5ca4acb3645e2f63509ae1f8
ShpagonovDmitriy <shpagonov.da@mail.ru>
2022-09-13 21:19:26 +0300

    add numeration on server side, del ol from html-page, add requirements.txt

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ html = """
 <html>
     <head>
         <title>Messages</title>
+        <style>
+        li {
+        list-style-type: none;
+        }
+        </style>
     </head>
     <body>
         <h1>Form with text field</h1>
@@ -22,16 +27,17 @@ html = """
             <input type="text" id="messageText" autocomplete="off"/>
             <button>Send</button>
         </form>
-        <ol id='messages'>
-        </ol>
+        <ul id='messages'>
+        </ul>
         <script>
             var ws = new WebSocket("ws://localhost:8000/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
                 message_json = JSON.parse(event.data)
+                message_num = message_json['message_number']
                 message_out = message_json['send_message']
-                var content = document.createTextNode(message_out)
+                var content = document.createTextNode(message_num + ". " + message_out)
                 message.appendChild(content)
                 messages.appendChild(message)
             };
@@ -56,8 +62,11 @@ async def root():
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    message_number = 0
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
+        message_number += 1
         message = json.loads(data)
+        message['message_number'] = message_number
         await websocket.send_text(json.dumps(message))
